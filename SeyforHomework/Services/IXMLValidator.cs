17ebc3474ac5d6da51369eade14732b9f10cded8
adam-szedely <dmszedely@gmail.com>
2023-01-24 10:59:42 +0100

    Added services + XSD

---
+++ SeyforHomework/Services/IXMLValidator.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace SeyforHomework.Services
+{
+	public interface IXMLValidator
+	{
+		public bool IsValidXML(string filePath);
+	}
+}
+
---
+++ SeyforHomework/Services/XMLValidator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace SeyforHomework.Services
+{
+	public class XMLValidator : IXMLValidator
+	{
+		public XMLValidator()
+		{
+		}
+
+        public bool FileExists(string filePath)
+        {
+            return File.Exists(filePath);
+        }
+
+        public bool IsValidXML(string filePath)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
