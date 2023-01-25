08375d80c08bef02a7b979768e85ba852e4bd3e2
adam-szedely <dmszedely@gmail.com>
2023-01-25 09:16:17 +0100

    Simplified validation service + added unit tests skeleton

---
+++ SeyforHomework/Services/ValidationHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Xml.Linq;
+using System.Xml.Schema;
+
+namespace SeyforHomework.Services
+{
+	public class XMLValidator : IXMLValidator
+	{
+		public XMLValidator()
+		{
+		}
+
+        public bool FileExists(string filePath)
+        {
+            return File.Exists(filePath);
+        }
+
+        public bool IsValidXML(string filePath)
+        {
+            return true;
+        }
+    }
+}
+
